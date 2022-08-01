7adde39b8e199b2856958b9710c158b66ae7dda0
GauravSehrawat <gauravsehrawt9813@gmail.com>
2022-08-02 00:47:48 +0530

    all changes done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import { useState } from 'react';
 
 function App() {
+  const [initial,setInitial]=useState("")
+   
+  const handleClick=(e)=>{
+    setInitial(initial.concat(e.target?.name));
+    
+  
+  }
+  const allClear=()=>{
+    setInitial("")
+  } 
+  const deleteNo=()=>{
+    setInitial(initial?.slice(0,-1));
+  }
+  const cal=()=>{
+    setInitial(eval(initial).toString())
+  }
+  const per=()=>{
+    let temp=initial/100;
+    setInitial(temp);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar/>
+     <div className="main">
+      <input type="text" className="diplay" value={initial} placeholder="0"/>
+      <div className="buttons">
+        
+        <button  onClick={allClear}  className="btn Clear">AC</button>
+        <button onClick={deleteNo}  className="btn back">Del</button>
+        <button onClick={per}  name="%" className="btn percentage">%</button>
+        <button onClick={handleClick}  name="/" className="btn divide">/</button>
+        <button onClick={handleClick}  name="7" className="btn seven">7</button>
+        <button onClick={handleClick} name="8" className="btn eight">8</button>
+        <button onClick={handleClick} name="9" className="btn nine">9</button>
+        <button onClick={handleClick} name="*" className="btn multiply">*</button>
+        <button onClick={handleClick} name="4" className="btn four">4</button>
+        <button onClick={handleClick} name="5" className="btn five">5</button>
+        <button onClick={handleClick} name="6 " className="btn six">6</button>
+        <button onClick={handleClick} name="-" className="btn minus">-</button>
+        <button onClick={handleClick} name="1" className="btn one">1</button>
+        <button onClick={handleClick} name="2" className="btn two">2</button>
+        <button onClick={handleClick} name="3" className="btn three">3</button>
+        <button onClick={handleClick} name="+" className="btn plus">+</button>
+        <button onClick={handleClick} name="0" className="btn zero">0</button>
+        <button onClick={handleClick} name="." className="btn point">.</button>
+        <button onClick={cal} className="btn equal">=</button>
+ 
+      </div>
+     </div>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+const NavBar = () => {
+  return (
+    <div className='nav'>
+        <img src='https://media-exp1.licdn.com/dms/image/C4D0BAQG-XkOD-nqQLw/company-logo_200_200/0/1656401897654?e=1667433600&v=beta&t=R0poFY5qe98N2aN5Id0DXgjWKQoPxV8FG6XCTXGUUdQ'/>
+        <h1>Calculator</h1>
+        
+
+    </div>
+  )
+}
+
+export default NavBar
